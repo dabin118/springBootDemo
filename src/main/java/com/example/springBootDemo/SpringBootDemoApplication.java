3f94e5560cfa5d74f6c839eed4f885cc22d15677
dabin118 <pjhuangwenbin@163.com>
2020-07-09 23:04:12 +0800

    init

---
+++ src/main/java/com/example/springBootDemo/SpringBootDemoApplication.java
@@ -0,0 +1,15 @@
+package com.example.springBootDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//本身就是一个组件
+
+@SpringBootApplication
+public class SpringBootDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDemoApplication.class, args);
+	}
+
+}
